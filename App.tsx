89ac21a85a749c66da1c4c44605d7290ac675167
saadchanna-09 <saadchanna786110@gmail.com>
2026-06-15 06:30:54 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -36,7 +36,7 @@ export default function App() {
     setData(null);
 
     try {
-      const response = await fetch('https://viziaudit-backend.vercel.app', {
+      const response = await fetch('https://viziaudit-backend.vercel.app/api/audit"', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url })
